28803645dd193ff657f3f432e0d92b4debcd1048
janezhuang <janezhuang@tencent.com>
2023-02-01 23:22:17 +0800

    Initial Commit

---
+++ JSONModel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JSONModel
+//
+//  Created by janezhuang on 2023/2/1.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
